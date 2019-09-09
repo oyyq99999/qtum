e4f0c55b8980d3d69fe73a87358f6b9964f6c9dd
Yunqi Ouyang <oyyq9999999@163.com>
2019-09-09 14:59:32 +0800

    configure travis for 0.18

--- .travis/test_04_install.sh
+++ .travis/test_04_install.sh
@@ -18,21 +18,15 @@ export LSAN_OPTIONS="suppressions=${TRAVIS_BUILD_DIR}/test/sanitizer_suppression
 export TSAN_OPTIONS="suppressions=${TRAVIS_BUILD_DIR}/test/sanitizer_suppressions/tsan:log_path=${TRAVIS_BUILD_DIR}/sanitizer-output/tsan"
 export UBSAN_OPTIONS="suppressions=${TRAVIS_BUILD_DIR}/test/sanitizer_suppressions/ubsan:print_stacktrace=1:halt_on_error=1"
 env | grep -E '^(BITCOIN_CONFIG|CCACHE_|WINEDEBUG|LC_ALL|BOOST_TEST_RANDOM|CONFIG_SHELL|(ASAN|LSAN|TSAN|UBSAN)_OPTIONS)' | tee /tmp/env
-if [[ $HOST = *-mingw32 ]]; then
-  DOCKER_ADMIN="--cap-add SYS_ADMIN"
-elif [[ $BITCOIN_CONFIG = *--with-sanitizers=*address* ]]; then # If ran with (ASan + LSan), Docker needs access to ptrace (https://github.com/google/sanitizers/issues/764)
-  DOCKER_ADMIN="--cap-add SYS_PTRACE"
-fi
+DOCKER_ADMIN="--cap-add SYS_PTRACE"
 DOCKER_ID=$(docker run $DOCKER_ADMIN -idt --mount type=bind,src=$TRAVIS_BUILD_DIR,dst=$TRAVIS_BUILD_DIR --mount type=bind,src=$CCACHE_DIR,dst=$CCACHE_DIR -w $TRAVIS_BUILD_DIR --env-file /tmp/env $DOCKER_NAME_TAG)
 
 DOCKER_EXEC () {
   docker exec $DOCKER_ID bash -c "cd $PWD && $*"
 }
 
-if [ -n "$DPKG_ADD_ARCH" ]; then
-  DOCKER_EXEC dpkg --add-architecture "$DPKG_ADD_ARCH"
-fi
-
-travis_retry DOCKER_EXEC apt-get update
-travis_retry DOCKER_EXEC apt-get install --no-install-recommends --no-upgrade -qq $PACKAGES $DOCKER_PACKAGES
-
+travis_retry DOCKER_EXEC apt-get update -qq
+travis_retry DOCKER_EXEC apt-get install -qq --no-install-recommends --no-upgrade $PACKAGES $DOCKER_PACKAGES
+travis_retry DOCKER_EXEC add-apt-repository -y ppa:bitcoin/bitcoin
+travis_retry DOCKER_EXEC apt-get update -qq
+travis_retry DOCKER_EXEC apt-get install -qq libdb4.8++-dev
--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -6,17 +6,15 @@
 
 export LC_ALL=C.UTF-8
 
-cd "build/bitcoin-$HOST" || (echo "could not enter distdir build/bitcoin-$HOST"; exit 1)
-
 if [ "$RUN_UNIT_TESTS" = "true" ]; then
   BEGIN_FOLD unit-tests
-  DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib make $MAKEJOBS check VERBOSE=1
+  DOCKER_EXEC make $MAKEJOBS check VERBOSE=1
   END_FOLD
 fi
 
 if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
   BEGIN_FOLD functional-tests
-  DOCKER_EXEC test/functional/test_runner.py --ci --combinedlogslen=4000 --coverage --quiet --failfast
+  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=500 --failfast --coverage --quiet --exclude wallet_txn_clone,rpc_psbt,wallet_multiwallet,feature_block,qtum_ignore_mpos_participant_reward,qtum_pos_conflicting_txs,qtum_evm_globals,qtum_duplicate_stake,qtum_callcontract_timestamp
   END_FOLD
 fi
 
